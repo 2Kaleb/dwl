2a109c898ae5fd8a67f39d1775113fad92ac1293
Kaleb Debre <kalebdebre@web.de>
2024-03-23 18:27:37 +0100

    update

--- config.mk
+++ config.mk
@@ -4,12 +4,12 @@ VERSION  = `git describe --tags --dirty 2>/dev/null || echo $(_VERSION)`
 PKG_CONFIG = pkg-config
 
 # paths
-PREFIX = /usr/local
+PREFIX = $(HOME)/.local
 MANDIR = $(PREFIX)/share/man
 DATADIR = $(PREFIX)/share
 
 XWAYLAND =
 XLIBS =
 # Uncomment to build XWayland support
-#XWAYLAND = -DXWAYLAND
-#XLIBS = xcb xcb-icccm
+XWAYLAND = -DXWAYLAND
+XLIBS = xcb xcb-icccm
